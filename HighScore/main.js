bf416267c01cd753b56177591f82df0237bf0222
JayvonDownerACS4 <jayvondowner@gmail.com>
2017-08-08 12:00:42 -0700

    Rock Paper Scissor

--- HighScore/main.js
+++ HighScore/main.js
@@ -1,58 +1,83 @@
+var playerScore = 0;
+var cpuScore = 0; 
 var cpuChoice = Math.random();
-var prompt = require('prompt-sync')();
-var playerChoice = prompt("Do you choose","rock, paper or scissors: ");    
+var playerChoice = ""; 
 
-// function setchoice(choice)
-// {
-//     console.log(this)
-// }
-
-var scissors = cpuChoice < 0.34;
-var paper = cpuChoice < 0.67;
-var rock = cpuChoice > 0.68;
-
-//Player choose Rock
-if (playerChoice == "rock" && cpuChoice == paper)
-    {
-        // console.log("CPU wins");
-        $("body").append("<h1>  CPU wins </h1>")
-    }
-
-if (playerChoice == "rock" && cpuChoice == scissors)
-    {
-        // console.log("Rock wins");
-        $("body").append("<h1> Player wins </h1>")
-    }
-
-if (playerChoice == scissors || playerChoice == paper || playerChoice == rock)
-    {
-        // console.log("It's a tie");
-        $("body").append("<h1>  It's a tie </h1>")
-    }
-
-//Player choose Paper
-if (playerChoice == "paper" && cpuChoice == scissors)
-    {
-        // console.log("CPU wins");
-        $("body").append("<h1>  CPU wins </h1>")
-    }
-
-if (playerChoice == "paper" && cpuChoice == rock)
-    {
-        // console.log("Player wins");
-        $("body").append("<h1>  Player wins </h1>")
-    }
-
-
-//Player choose Scissors
-if (playerChoice == "s" && cpuChoice == paper)
+function setChoice(choice)
 {
-    // console.log("Player's Scissors beats CPU's Paper");
-    $("body").append("<h1>  Player wins </h1>")
+    playerChoice= choice;
+    console.log(playerChoice);
+    $("body").append("<h1> Player: " + playerChoice+ "</h1>");
 }
 
-if (playerChoice == "s" && cpuChoice == rock)
+
+console.log(cpuChoice);
+function playRps()
 {
-    // console.log("Player's Scissors beats CPU's Rock");
-    $("body").append("<h1>  CPU wins </h1>")
+    var cpuChoice = Math.random();
+    if (cpuChoice <= 0.34)
+    {
+        cpuChoice = "scissors";
+
+    }
+    if (cpuChoice <= 0.67)
+    {
+        cpuChoice = "paper";
+    }
+    if (cpuChoice >= 0.68)
+    {
+        cpuChoice = "rock";
+    }
+    $("body").append("<h1> CPU: " + cpuChoice+ "</h1>");
+
+
+    //Player choose Rock    
+    if (playerChoice == cpuChoice )
+        {
+            $("body").append("<h1> It's a tie </h1>");
+        }
+
+    if (playerChoice == "rock" && cpuChoice == "scissors")
+    {
+            $("body").append("<h1> Player wins </h1>");
+            playerScore = playerScore + 1;
+            console.log("playerScore = " + playerScore);
+    }
+    if (playerChoice == "rock" && cpuChoice == "paper")
+    {
+            $("body").append("<h1> Cpu Wins </h1>");
+            cpuScore = cpuScore + 1;
+            console.log("cpuScore = " + cpuScore);
+    }
+
+    //Player choose Paper
+    if (playerChoice == "paper" && cpuChoice == "scissors")
+        {
+            $("body").append("<h1>  CPU wins </h1>");
+            cpuScore = cpuScore + 1;
+            console.log("cpuScore = " + cpuScore);
+        }
+
+    if (playerChoice == "paper" && cpuChoice == "rock")
+        {
+            $("body").append("<h1>  Player wins </h1>");
+            playerScore = playerScore + 1;
+            console.log("playerScore = " + playerScore);
+        }
+
+    //Player choose Scissors
+    if (playerChoice == "scissors" && cpuChoice == "paper")
+    {
+        $("body").append("<h1>  Player wins </h1>");
+        playerScore = playerScore + 1;
+        console.log("playerScore = " + playerScore);
+    }
+
+    if (playerChoice == "scissors" && cpuChoice == "rock")
+    {
+        $("body").append("<h1>  CPU wins </h1>");
+        cpuScore = cpuScore + 1;
+        console.log("cpuScore = " + cpuScore);
+    }
 }
+
